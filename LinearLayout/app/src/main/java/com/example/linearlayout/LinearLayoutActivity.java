d8bfa776bfe6abbefc31ffbd1de6c23fdc6a9d64
Akilan Mohan <akilan.mohan@sc.com>
2017-01-11 12:11:40 +0800

    Pushing Linear Layout with text field and button Android program

---
+++ LinearLayout/app/src/main/java/com/example/linearlayout/LinearLayoutActivity.java
@@ -0,0 +1,17 @@
+package com.example.linearlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Akhilan on 1/11/2017.
+ */
+
+public class LinearLayoutActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
