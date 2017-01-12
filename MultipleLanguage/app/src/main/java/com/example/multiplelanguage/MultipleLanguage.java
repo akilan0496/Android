625ec77f8efc6d13c5e7de5a55c2c0b5e565330a
Akilan Mohan <akilan.mohan@sc.com>
2017-01-12 12:45:58 +0800

    Pushing sample for supporting multiple language

---
+++ MultipleLanguage/app/src/main/java/com/example/multiplelanguage/MultipleLanguage.java
@@ -0,0 +1,17 @@
+package com.example.multiplelanguage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Akhilan on 1/11/2017.
+ */
+
+public class MultipleLanguage extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiple_language);
+    }
+}
