0cabdaec64dca21f08386ed7ea002b56931ba88a
Akilan Mohan <akilan.mohan@sc.com>
2017-01-16 14:54:42 +0800

    pishing multiple screen layout sample

---
+++ MultipleScreenLayout/app/src/main/java/com/example/multiplescreenlayout/MultipleScreenLayout.java
@@ -0,0 +1,18 @@
+package com.example.multiplescreenlayout;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Akhilan on 1/16/2017.
+ */
+
+public class MultipleScreenLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiple_screen_layout);
+    }
+}
